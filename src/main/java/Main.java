2e768102bfc569b08ad6c595ffd72e0407fa5fa2
yulkinpa.24 <iulkinpasha@gmail.com>
2022-12-01 11:32:03 +0300

    events consumer works

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+
+import app2.Application;
+import io.github.humbleui.jwm.App;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        App.start(Application::new);
+    }
+
+}
---
+++ src/main/java/app2/Application.java
@@ -0,0 +1,13 @@
+package app2;
+
+import io.github.humbleui.jwm.Event;
+
+import java.util.function.Consumer;
+
+public class Application implements Consumer<Event> {
+
+    @Override
+    public void accept(Event event) {
+
+    }
+}
