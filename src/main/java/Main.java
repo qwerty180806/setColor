d974659be75db8aeda9abcd00bf9c1fc84a8fe68
yulkinpa.24 <iulkinpasha@gmail.com>
2022-12-01 11:56:46 +0300

    java doc

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,16 @@
-
-import app2.Application;
+import app.Application;
 import io.github.humbleui.jwm.App;
 
+/**
+ * Главный класс приложения
+ */
 public class Main {
-
+    /**
+     * Главный метод приложения
+     *
+     * @param args аргументы командной строки
+     */
     public static void main(String[] args) {
-
         App.start(Application::new);
     }
-
 }
